6f818c467572b5ff274433065128f1b5ffc77c61
Steve Rhoton <sprhoto@gmail.com>
2021-03-12 20:41:14 -0500

    It works, just need to refactor and create deploy and test scripts

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,22 +21,158 @@ resource "aws_vpc" "default_vpc" {
   cidr_block = "172.31.0.0/16"
 }
 
-resource "aws_subnet" "default_subnet_one" {
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default_vpc.id
+}
+
+resource "aws_subnet" "blue_subnet" {
   vpc_id     = aws_vpc.default_vpc.id
   cidr_block = "172.31.0.0/20"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Default Subnet One"
+    Name = "Default Subnet Blue"
   }
 }
 
-resource "aws_subnet" "default_subnet_two" {
+resource "aws_subnet" "red_subnet" {
   vpc_id     = aws_vpc.default_vpc.id
-  cidr_block = "172.31.48.0/20"
+  cidr_block = "172.31.32.0/20"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Default Subnet One"
+    Name = "Default Subnet Red"
+  }
+}
+
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "teletracking-ec2-sg"
+  description = "Security group for Teletracking EC2 instances"
+  vpc_id      = aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  name        = "teletracking-alb-sg"
+  description = "Security group for Teletracking ALB"
+  vpc_id      = aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "template_file" "blue_data" {
+  template = file("./blue_data.tpl")
+}
+
+resource "aws_instance" "blue_machine" {
+  ami           = "ami-0ca5c3bd5a268e7db"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  subnet_id  = aws_subnet.blue_subnet.id
+  key_name = "teletracking-demo"
+  user_data = data.template_file.blue_data.rendered
+}
+
+data "template_file" "red_data" {
+  template = file("./red_data.tpl")
+}
+
+resource "aws_instance" "red_machine" {
+  ami           = "ami-0ca5c3bd5a268e7db"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  subnet_id  = aws_subnet.red_subnet.id
+  key_name = "teletracking-demo"
+  user_data = data.template_file.red_data.rendered
+}
+
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = "teletracking-demo-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.default_vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "blue_attachment" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.blue_machine.id
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "red_attachment" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.red_machine.id
+  port             = 8080
+}
+
+resource "aws_lb" "teletracking_lb" {
+  name               = "teletracking-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [aws_subnet.blue_subnet.id,aws_subnet.red_subnet.id]
+
+  tags = {
+    Environment = "teletracking-demo"
+  }
+}
+
+resource "aws_lb_listener" "teletracking_listener" {
+  load_balancer_arn = aws_lb.teletracking_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
---
+++ infrastructure/red_data.tpl
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "starting user config"
+
+export DEBIAN_FRONTEND=noninteractive
+echo "doing update"
+apt-get -qy update
+echo "doing upgrade"
+apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade
+echo "doing docker install"
+apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" install docker.io
+echo "enabling docker"
+systemctl enable docker
+echo "starting docker"
+systemctl start docker
+echo "pulling image"
+docker pull sprhoto/red-machine:latest
+echo "running image"
+docker run -d --name red-machine -p 8080:80 sprhoto/red-machine:latest
+echo "Completed." 
